be94860e6f360034e61f47e1e3b190fd8b6a8463
WillyChen <w0804559@gmail.com>
2024-03-13 11:38:57 +0800

    updata01

--- add_project01.py
+++ add_project01.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import jieba
+import os
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
